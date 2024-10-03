653ccc4408cd08bb6b792b4d9009dfe2de84f69d
AkibHossain14 <akibhossain2523@gmail.com>
2024-10-03 23:22:03 +0600

    irf statement

---
+++ if_statement.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    cout<< " enter any  number " <<endl;
+    float num;
+
+    cin>> num;
+    if (num<0){
+        cout<< num <<" = s negative " ;
+    }
+    if (num>0) {
+        cout<< num <<" = number  is positive" ;
+    }
+    if ( num==0) {
+        cout<< num << " = Number is zero" ;
+    }
+
+
+
+
+
+
+
+
+    getch();
+}
+
