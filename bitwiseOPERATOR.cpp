bdfdc32de10562559e59a2c3305c8b444773b3ad
AkibHossain14 <akibhossain2523@gmail.com>
2024-10-03 17:01:53 +0600

    practising bitwise  operator

---
+++ bitwiseOPERATOR.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main ()
+{
+    int x=32,y,z;
+
+     y=x>>2;
+
+    cout<< y <<endl;
+    z=x<<2;
+    cout << z;
+
+
+
+
+
+
+
+
+
+
+    getch ();
+}
