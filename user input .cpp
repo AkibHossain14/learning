8edc8d24bf8db3af2bd9dad57f9501a062802589
AkibHossain14 <akibhossain2523@gmail.com>
2024-10-03 15:41:59 +0600

    taking user inpuut

---
+++ user input .cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main ()
+{
+    int num ;
+    cout<< " please enter your phone  number "  <<endl;
+     cin >> num ;
+     char name [20];
+     cout<< " please enter your name " <<endl;
+     cin>> name ;
+     cout<< " your phone number is " <<num <<endl;
+     cout<< " your name is " << name ;
+
+
+
+     getch ();
+}
+
+
