bcb8c7187bf6e3ba7f176b7083e5bb90790798ee
AkibHossain14 <akibhossain2523@gmail.com>
2024-10-10 05:59:10 +0600

    recording while loop

--- WHILE_LOOP.cpp
+++ WHILE_LOOP.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+ int main()
+ {
+     int num;
+     cout<< "please enter any interger number ";
+     cin>> num;
+
+     int i=1;
+     while (i<=num){
+
+        cout<< " CPP is awesome";
+        i++;
+        cout<<endl;
+     }
+
+
+
+
+
+
+
+
+     getch();
+ }
 
