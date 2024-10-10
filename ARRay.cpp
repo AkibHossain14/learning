e402149aceb10cdeb70b3608a97236ae8f0a88f8
AkibHossain14 <akibhossain2523@gmail.com>
2024-10-10 06:31:02 +0600

    solved the problem

--- ARRay.cpp
+++ ARRay.cpp
@@ -6,10 +6,10 @@ using namespace std;
  {
 
      cout<< " here we have given you a list of students marks " <<endl;
-     int marks  [3] ={80,88,63};
+     int marks  [ ] ={80,88,63,90,96,45,44,34,69};
 
-     for (int i=0; i<3; i++){
-        cout<< "Roll no  "<<i+1 <<" marks is= " <<marks ;
+     for (int i=0; i<9; i++){
+        cout<< "Roll no  "<<i+1 <<" marks is= " <<marks [i] ;
         cout<<endl;
      }
 
