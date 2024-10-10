07d6a74dd3e5624a52bb497321a84cf0d218bba7
AkibHossain14 <akibhossain2523@gmail.com>
2024-10-10 06:26:02 +0600

    there is something wrong with this code

---
+++ ARRay.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+ int main()
+ {
+
+     cout<< " here we have given you a list of students marks " <<endl;
+     int marks  [3] ={80,88,63};
+
+     for (int i=0; i<3; i++){
+        cout<< "Roll no  "<<i+1 <<" marks is= " <<marks ;
+        cout<<endl;
+     }
+
+
+
+
+
+
+
+     getch();
+ }
