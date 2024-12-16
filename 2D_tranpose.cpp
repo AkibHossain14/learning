eff9cdc5b1f0cedb4385f36c7f35397c7a39496c
AkibHossain14 <akibhossain2523@gmail.com>
2024-12-16 11:25:36 +0600

    2d lower triangle

---
+++ 2D_tranpose.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+int main ()
+{
+
+    int arr[15][15],a,sum=0;
+
+    cout<< " enter the number of row and column in the matrix :" ;
+    cin >>a ;
+
+    arr[a] [a];
+
+    cout<< " enter the elements of the matrix :" <<endl ;
+    for (int i =0 ; i <a ; i++)
+    {
+        for (int  j=0 ; j <a ; j++)
+        {
+            cout<< " Array [" <<i <<"] [" <<j <<"] :" ;
+            cin >>arr[i] [j] ;
+
+        }
+    }
+
+    cout <<endl <<endl << " your matrix is :" <<endl ;
+
+    for (int i =0 ; i <a ; i++)
+    {
+        for (int  j=0 ; j <a ; j++)
+        {
+            cout<< "\t" << arr[i] [j];
+
+        }
+        cout<<endl;
+    }
+
+
+
+    cout<<endl <<  " your transpose matrix is  :";
+
+
+    for (int i =0 ; i <a ; i++)
+    {
+        for (int  j=0 ; j <a ; j++)
+        {
+              cout<< "\t" << arr[j] [i];
+
+        }
+       cout<<endl;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    getch ();
+}
+
