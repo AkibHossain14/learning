ff7a9fab6496bd76cc1b95602ac754abf1f023c5
AkibHossain14 <akibhossain2523@gmail.com>
2024-12-16 11:21:41 +0600

    2d lower triangle

---
+++ 2D_Explicit.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+int main ()
+{
+
+    int arr[15][15],a,sum=0;
+
+    cout<< " enter the number of row and column in the matrix :" ;
+    cin >>a ;
+
+    arr[a] [a];
+
+    cout<< " enter the elements of the matrix :" <<endl ;
+    for (int i =0 ; i <a ; i++)
+    {
+        for (int  j=0 ; j <a ; j++)
+        {
+            cout<< " Array [" <<i <<"] [" <<j <<"] :" ;
+            cin >>arr[i] [j] ;
+
+        }
+    }
+
+    cout <<endl <<endl << " your matrix is :" <<endl ;
+
+    for (int i =0 ; i <a ; i++)
+    {
+        for (int  j=0 ; j <a ; j++)
+        {
+            cout<< "\t" << arr[i] [j];
+
+        }
+        cout<<endl;
+    }
+
+
+
+    cout<<endl <<  " your lower triangle elements are :";
+
+
+    for (int i =0 ; i <a ; i++)
+    {
+        for (int  j=0 ; j <a ; j++)
+        {
+            if (i>j)
+            {
+                cout<< arr[i][j] << " " ;
+                sum = sum + arr[i] [j];
+            }
+
+        }
+
+
+    }
+
+
+    cout<<endl << " sum of the lower elements  is " << sum ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    getch ();
+}
+
